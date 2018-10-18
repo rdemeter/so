0535abfc6916b4c70d1972f2ab156960825adf76
rdemeter <35566820+rdemeter@users.noreply.github.com>
2018-10-18 16:58:38 +0300

    Update io-wrapper.c

--- lab2/io-wrapper.c
+++ lab2/io-wrapper.c
@@ -1,14 +1,15 @@
 #include "io-wrapper.h"
 #ifdef __linux__
-
+// todo: os_open()
 os_ssize os_read(os_handle fd, void *buffer, os_size count) {
   return read(fd, buffer, count);
 }
 os_ssize os_write(os_handle fd, const void *buffer, os_size count) {
   return write(fd, buffer, count);
 }
+//todo: os_close()
 #elif defined(_WIN32)
-
+// todo: os_open
 os_ssize os_read(os_handle fd, void *buffer, os_size count) {
   os_ssize result = -1;
   ReadFile(fd, buffer, count, &result, NULL);
@@ -19,12 +20,14 @@ os_ssize os_write(os_handle fd, void *buffer, os_size count) {
   WriteFile(fd, buffer, count, &result, NULL);
   return result;
 }
+// todo: os_close()
 #endif
 
 int main(void) {
-  os_handle fd=os_open();
-  os_read(fd, buf, 10);
-  printf("%s\n",buf); 
+  char buf[101];
+  os_handle fd = os_open(filename);
+  os_read(fd, buf, 100);
+  printf("buf=%s\n",buf); 
   os_close(fd);
 }
 
