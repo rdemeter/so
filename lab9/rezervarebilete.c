d113948164ea9d45f51bb8f4425cf29312e1a5d0
rdemeter <35566820+rdemeter@users.noreply.github.com>
2018-10-19 15:54:36 +0300

    Update rezervarebilete.c

--- lab9/rezervarebilete.c
+++ lab9/rezervarebilete.c
@@ -4,8 +4,10 @@
 #include <time.h>
 #include <pthread.h>
 #include <semaphore.h>
+
 #define NUM_SELLERS 5  //50
 #define NUM_TICKETS 25 //250000
+
 static int numTickets = NUM_TICKETS;
 
 void *sellerThread(void* arg)
@@ -17,21 +19,28 @@ void *sellerThread(void* arg)
     total++;
     printf("Seller %ld sold one (%d left)\n", (long) arg, numTickets);
   } while(numTickets > 0);
+
   printf("Seller %ld finished! (I sold %d tickets)\n", (long) arg, total);
+
   pthread_exit((void*) total);
 }
+
 int main(void)
 {
   long i;
   int total = 0;
+
   pthread_t tids[NUM_SELLERS];
+
   for(i=0; i < NUM_SELLERS; i++)
     pthread_create(&tids[i], NULL, sellerThread, (void*) i);
+
   for(i=0; i < NUM_SELLERS; i++) {
     int value;
     pthread_join(tids[i], (void**)&value);
     total += value;
   }
+
   printf("Done %d!\n", total);
   return 0;
 }
