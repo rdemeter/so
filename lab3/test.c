8e8b1462bea3ab440b826f806dbfe987f20270b2
rdemeter <35566820+rdemeter@users.noreply.github.com>
2018-10-19 12:44:08 +0300

    Add files via upload

---
+++ lab3/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <unistd.h> 
+int main(void) 
+{ 
+    printf("A\n"); 
+    fork(); 
+    printf("B\n"); 
+    fork(); 
+    printf("C\n"); 
+    fork(); 
+    printf("D\n"); 
+    return 0; 
+} 
