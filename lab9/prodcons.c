6f00969f7736c9fb769f3ed72bd2fe62a43bfb6b
rdemeter <35566820+rdemeter@users.noreply.github.com>
2018-10-19 15:01:29 +0300

    Update prodcons.c

--- lab9/prodcons.c
+++ lab9/prodcons.c
@@ -4,9 +4,12 @@
 #include <time.h>
 #include <pthread.h>
 #include <semaphore.h>
+
 #define NUM_TOTAL_BUFFERS 5
 #define DATA_LENGTH 20
+
 char buffers[NUM_TOTAL_BUFFERS];
+
 void *producer()
 {
   int i, writePt = 0;
@@ -31,12 +34,15 @@ void* consumer()
     readPt = (readPt + 1) % NUM_TOTAL_BUFFERS;
   }
 }
+
 int main(void) {
   pthread_t prod, cons;
   pthread_create(&prod, NULL, producer, NULL);
   pthread_create(&cons, NULL, consumer, NULL);
+
   pthread_join(prod, NULL);
   pthread_join(cons, NULL);
+
   printf("done!\n");
   return 0;
 }
