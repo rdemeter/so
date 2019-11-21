608dbe229fa22ff36021a048b2490a8419d203a6
rdemeter <35566820+rdemeter@users.noreply.github.com>
2019-11-21 14:21:34 +0200

    Add files via upload

---
+++ lab6/sigint2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<signal.h>
+
+void signalHandler( int signum ) {
+   printf("Interrupt signal %d received.\n", signum);
+
+   // cleanup and close up stuff here
+   // terminate program
+
+   //exit(signum);
+}
+
+int main () {
+   int i = 0;
+   // register signal SIGINT and signal handler
+   signal(SIGINT, signalHandler);
+
+   while(++i) {
+      printf("Going to sleep....\n");
+      if( i == 3 ) {
+         raise( SIGINT);
+      }
+      sleep(1);
+   }
+
+   return 0;
+}
