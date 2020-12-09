31961eca41aaf80388cacb203e06313d71aa53ef
rdemeter <35566820+rdemeter@users.noreply.github.com>
2020-12-09 09:28:24 +0200

    Update ex_thread.c

--- lab7/ex_thread.c
+++ lab7/ex_thread.c
@@ -8,16 +8,19 @@ void* thread_function(void)
 {
   char *a = malloc(12);
   strcpy(a, "hello world");
-  //sleep(10)
+  //sleep(10);
   pthread_exit((void*)a);
 }
 
 int main()
 {
   pthread_t thread_id;
-  char *b;
+  char *b = NULL;
   pthread_create (&thread_id, NULL, (void*)&thread_function, NULL);
 
+  //
+  //While the created thread is running, the main thread can perform other tasks.
+  
   //here we are reciving one pointer value so to use that we need double pointer
   pthread_join(thread_id,(void**)&b);
 
