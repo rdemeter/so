d3f7a7051a7169afd15563a0b079ee557f015a94
rdemeter <35566820+rdemeter@users.noreply.github.com>
2022-10-20 14:37:06 +0300

    Create is_positiv.sh

---
+++ lab0/is_positiv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Script pentru a observa daca argumentul este pozitiv sau negativ
+#
+if [ $# -eq 0 ]
+then
+    echo "$0 : Trebuie sa introduceti un intreg"
+exit 1
+fi
+
+if test $1 -ge 0
+then
+    echo " $1 este un numar pozitiv"
+else
+    echo " $1 este un numar negativ"
+fi
