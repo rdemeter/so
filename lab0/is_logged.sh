35fc834f33f1e8b91b1ba47fe1fd357221cf79a2
rdemeter <35566820+rdemeter@users.noreply.github.com>
2022-10-20 11:20:03 +0300

    Create is_logged.sh

---
+++ lab0/is_logged.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# 0 - user was found
+# 1 - wrong argument
+# 2 - user not found
+
+if test"$#" -eq 1
+then
+    if who -a | tr -s ' ' | cut -d ' ' -f 1 | grep "^$1$" > /dev/null
+    then
+        echo "User $1 is logged-in."
+    else
+        echo "User $1 is not logged-in."
+        exit 2
+    fi
+else
+    echo "Usage: $0 <user>"
+    exit 1
+fi
