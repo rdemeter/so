1761af411bc4eb2b502aa0b482ced347e8d5139d
rdemeter <35566820+rdemeter@users.noreply.github.com>
2023-10-04 16:40:03 +0300

    Create fifoserver.c

---
+++ lab5/fifoserver.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <linux/stat.h>
+
+#define  FIFO_FILE  "myfifo"
+#define  SZBUF  80
+#define  QUIT  "bye"
+
+int main(void)
+{
+    FILE* fp;
+    char readbuf[SZBUF];
+
+    /*create the fifo*/
+    umask(0);
+    mknod(FIFO_FILE, S_IFIFO|0666, 0);
+
+    printf("Running server... To quit, send via the client the msg 'bye'.\n");
+
+    while(1) {
+        fp = fopen(FIFO_FILE,"r");
+        fgets(readbuf, SZBUF, fp);
+
+        if (!strcmp(readbuf,QUIT)) break;
+
+        printf("Received string: %s\n",readbuf);
+        fclose(fp);
+    }
+    return 0;
+}
